15ef5bc6d49ddf66715bf926591fa3e5e0577e87
sgh4123@naver.com <sgh4123@naver.com>
2024-10-31 02:22:58 +0900

    ✨ Feat(CreateSubnetResponse 정의 구현):

--- packages/cloud-sdk/src/services/subnet/models/CreateSubnetResponse.ts
+++ packages/cloud-sdk/src/services/subnet/models/CreateSubnetResponse.ts
@@ -1,4 +1,21 @@
+import { Subnet } from './Subnet';
+
 /**
  * 서브넷 생성을 위한 응답 객체
  */
-export interface CreateSubnetResponse {}
+export interface CreateSubnetResponse {
+    /**
+     * 조회된 목록의 총 개수.
+     * 페이징 처리 요청의 경우 전체 개수.
+     *
+     * Required
+     */
+    totalRows: number;
+
+    /**
+     * List<Subnet> 데이터 타입.
+     *
+     * Required
+     */
+    subnetList: Subnet[];
+}

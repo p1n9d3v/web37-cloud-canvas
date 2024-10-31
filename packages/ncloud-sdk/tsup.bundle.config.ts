2ef12a448bfeb88a1185a034a295f0176da361d7
paulcjy <paulcjy@naver.com>
2024-10-31 21:28:43 +0900

    🐞 Fix(ncloud-sdk): ncloud-sdk는 개별 패키지로 빌드하도록 설정

--- packages/ncloud-sdk/tsup.bundle.config.ts
+++ packages/ncloud-sdk/tsup.bundle.config.ts
@@ -2,7 +2,6 @@ import { defineConfig, type Options } from 'tsup';
 
 export default defineConfig((options: Options) => ({
     entryPoints: ['src/index.ts'],
-    outDir: '../cli/dist/server',
     minify: true,
     format: ['cjs', 'esm'],
     ...options,

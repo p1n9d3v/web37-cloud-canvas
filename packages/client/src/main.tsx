eae5bd31dd1a354c76287b75eaaa8199141b36e9
p1n9d3v <p1n9@p1n9s-MacBook-Air.local>
2024-11-04 20:23:20 +0900

    🚚 Setting(client): path alias(types,theme) 수정 및 추가

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { ThemeProvider, CssBaseline } from '@mui/material';
-import theme from './theme/index.ts';
+import theme from '@theme';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>

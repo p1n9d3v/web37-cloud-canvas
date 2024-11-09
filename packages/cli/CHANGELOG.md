1d56f7286491f11c72f1381c07e22f99b4326d25
sgh4123@naver.com <sgh4123@naver.com>
2024-11-09 10:30:06 +0900

    🚚 Setting: nest 설치 및 세팅 - 프로젝트 기존의 packages에서 apps로 주요 사용 패키지 옮김 - cli 패키지 삭제 - server 패키지 삭제 후 새로 다시 만듦
    Co-authored-by: paulcjy <paulcjy@naver.com>

--- packages/cli/CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-# cloud-canvas
-
-## 0.0.1
-
-### Patch Changes
-
--   초기 환경 설정
--- packages/server/tsup.bundle.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig, type Options } from 'tsup';
-
-export default defineConfig((options: Options) => ({
-    entryPoints: ['src/index.ts'],
-    outDir: '../cli/dist/server',
-    minify: true,
-    format: ['cjs', 'esm'],
-    ...options,
-}));

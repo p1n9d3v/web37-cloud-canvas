3b0f6943845b4f83a293a6d8db4aab1cc1513884
sgh4123@naver.com <sgh4123@naver.com>
2024-11-09 10:51:45 +0900

    🚚 Setting: server 패키지의 테스트 환경을 jest에서 vitest로 변경
    Co-authored-by: paulcjy <paulcjy@naver.com>

--- apps/server/test/app.e2e-spec.ts
+++ apps/server/test/app.e2e-spec.ts
@@ -1,7 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { describe, beforeEach, it} from 'vitest';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;

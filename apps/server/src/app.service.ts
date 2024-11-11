f9872e33ff33c845c9362008ebb3518087d84102
paulcjy <paulcjy@naver.com>
2024-11-11 22:22:11 +0900

    ✨ Feat(server): prisma ORM 설치 및 설정

--- apps/server/src/app.service.ts
+++ apps/server/src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+    constructor(private readonly prisma: PrismaService) {}
+
+    async getHello(): Promise<string> {
+        const data = await this.prisma.user.findMany();
+        return JSON.stringify(data);
+    }
 }

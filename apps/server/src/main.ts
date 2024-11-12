114b0c0f13c927c86b561d2596d91ea68ac087f2
paulcjy <paulcjy@naver.com>
2024-11-13 02:31:11 +0900

    ✨ Feat(server): global Valication Pipe 추가
    ISSUES CLOSED: #49

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { swaggerConfig } from './swagger/swagger.config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     swaggerConfig(app);
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            transform: true,
+            whitelist: true,
+            forbidNonWhitelisted: true,
+            skipMissingProperties: false,
+            validateCustomDecorators: true,
+            disableErrorMessages: false,
+        }),
+    );
 
     await app.listen(3000);
 }

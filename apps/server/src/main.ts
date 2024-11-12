4b12315db884f41a84edabfd924ee907b3cb9c16
paulcjy <paulcjy@naver.com>
2024-11-13 02:33:47 +0900

    ✨ Feat(server): nestJS에 helmet 적용

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -2,11 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { swaggerConfig } from './swagger/swagger.config';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     swaggerConfig(app);
+
+    app.use(helmet());
 
     app.useGlobalPipes(
         new ValidationPipe({

6be2831e498d0e113e258920cfd7743b6d8242c7
paulcjy <paulcjy@naver.com>
2024-11-14 00:11:23 +0900

    ✨ Feat(server): private 관련 dto에 validation 추가

--- apps/server/src/architecture/private-version/dto/create-private-version.dto.ts
+++ apps/server/src/architecture/private-version/dto/create-private-version.dto.ts
@@ -1,5 +1,15 @@
+import { IsNotEmpty, IsNumber, IsObject, IsString } from 'class-validator';
+
 export class CreatePrivateVersionDto {
+    @IsNumber()
+    @IsNotEmpty()
     privateArchitectureId: number;
+
+    @IsString()
+    @IsNotEmpty()
     title: string;
+
+    @IsObject()
+    @IsNotEmpty()
     architecture: Record<string, any>;
 }
--- apps/server/src/architecture/private/dto/create-private.dto.ts
+++ apps/server/src/architecture/private/dto/create-private.dto.ts
@@ -1,7 +1,15 @@
+import { IsNotEmpty, IsNumber, IsObject, IsString } from 'class-validator';
+
 export class CreatePrivateDto {
+    @IsString()
+    @IsNotEmpty()
     title: string;
+
+    @IsNumber()
+    @IsNotEmpty()
     authorId: number;
+
+    @IsObject()
+    @IsNotEmpty()
     architecture: Record<string, any>;
-    createdAt: Date;
-    updatedAt: Date;
 }

871c2e3d6b827bca16e33d418e5d0cd0d24d627d
paulcjy <paulcjy@naver.com>
2024-11-15 23:52:50 +0900

    ✅ Test(server): auth controller 테스트 코드 개선

--- apps/server/src/auth/auth.controller.spec.ts
+++ apps/server/src/auth/auth.controller.spec.ts
@@ -3,14 +3,16 @@ import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { vi } from 'vitest';
+import { AuthenticatedUser } from 'src/types/authenticated-user.interface';
 
 describe('AuthController', () => {
     let controller: AuthController;
+    let service: AuthService;
+
+    const mockUser = { id: 1, name: 'Test User' } as AuthenticatedUser;
 
     const mockAuthService = {
-        login: vi.fn().mockImplementation(() => {
-            return { access_token: 'ACCESS_TOKEN' };
-        }),
+        login: vi.fn().mockResolvedValue({ access_token: 'ACCESS_TOKEN' }),
     };
 
     beforeEach(async () => {
@@ -31,6 +33,7 @@ describe('AuthController', () => {
         }).compile();
 
         controller = module.get<AuthController>(AuthController);
+        service = module.get<AuthService>(AuthService);
     });
 
     it('AuthController는 정상적으로 정의되어야 한다.', () => {
@@ -38,50 +41,37 @@ describe('AuthController', () => {
     });
 
     describe('POST /auth/login', () => {
-        beforeAll(async () => {
-            await controller.login();
-        });
-
-        it('authService.login()을 호출해야 한다.', async () => {
-            expect(mockAuthService.login).toHaveBeenCalled();
-        });
-
-        it('authService.login()는 1번 호출되어야 한다.', async () => {
-            expect(mockAuthService.login).toHaveBeenCalledTimes(1);
-        });
-
-        it('authService.login()는 아무 인자 없이 호출되어야 한다.', async () => {
-            expect(mockAuthService.login.mock.calls[0]).toEqual([]);
-        });
-
-        it('로그인에 성공하면 authService.login()의 반환값을 그대로 반환해야 한다.', async () => {
-            expect(mockAuthService.login.mock.results[0].value).toEqual({
-                access_token: 'ACCESS_TOKEN',
-            });
-        });
-
-        it('authService.login이 다른 값을 반환해도 그대로 반환해야 한다.', async () => {
-            mockAuthService.login.mockReturnValueOnce({
-                access_token: 'DIFFERENT_TOKEN',
-                refresh_token: 'REFRESH_TOKEN',
-            });
+        it('사용자가 로그인에 성공하면 access token을 반환해야 한다.', async () => {
             const result = await controller.login();
-            expect(result).toEqual({
-                access_token: 'DIFFERENT_TOKEN',
-                refresh_token: 'REFRESH_TOKEN',
-            });
+
+            expect(service.login).toHaveBeenCalledWith();
+            expect(result).toEqual({ access_token: 'ACCESS_TOKEN' });
         });
-        it('로그인에 실패하면 에러가 발생해야 한다.', async () => {
+
+        it('service.login이 다른 값을 반환해도 그대로 반환해야 한다.', async () => {
+            const mockLoginResult = {
+                refresh_token: 'REFRESH_TOKEN',
+                access_token: 'DIFFERENT_TOKEN',
+            };
+            mockAuthService.login.mockReturnValueOnce(mockLoginResult);
+
+            const result = await controller.login();
+
+            expect(result).toEqual(mockLoginResult);
+        });
+
+        it('사용자가 로그인에 실패하면 에러가 발생해야 한다.', async () => {
             mockAuthService.login.mockRejectedValueOnce(new Error());
+
             await expect(controller.login()).rejects.toThrow();
         });
     });
 
     describe('GET /auth/check', () => {
-        it('로그인 된 사용자일 경우 사용자 정보를 반환해야 한다. ', () => {
-            const user = { id: 1, username: 'Test User' };
-            const result = controller.check(user as any);
-            expect(result).toEqual(user);
+        it('인증된 사용자일 경우 사용자 정보를 반환해야 한다. ', () => {
+            const result = controller.check(mockUser);
+
+            expect(result).toEqual(mockUser);
         });
     });
 });

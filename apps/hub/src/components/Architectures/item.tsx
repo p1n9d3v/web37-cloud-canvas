bda086c66c05c4edc97e09006494008aafa54f80
paulcjy <paulcjy@naver.com>
2024-11-20 13:40:35 +0900

    ✨ Feat(hub): 아키텍처 항목에 태그 추가
    ISSUES CLOSED: #121

--- apps/hub/src/components/Architectures/item.tsx
+++ apps/hub/src/components/Architectures/item.tsx
@@ -1,29 +1,39 @@
 import Link from 'next/link';
+import { Tag } from '../Tag';
 
 export const ArchitectureItem = ({
+    id,
     title,
     author,
     cost,
-    date,
+    createdAt,
     stars,
     imports,
+    tags,
 }: {
+    id: number;
     title: string;
     author: string;
     cost: number;
-    date: string;
+    createdAt: string;
     stars: number;
     imports: number;
+    tags: string[];
 }) => {
     return (
         <li className="hover:bg-gray-50 border-b flex px-3 py-2">
             <div className="flex flex-col basis-7/12">
                 <div>
-                    <Link href="#">{title}</Link>
+                    <Link href={`/architectures/${id}`}>{title}</Link>
                 </div>
                 <div className="text-xs text-gray-400 flex">
-                    <div>{date}</div>
+                    <div>{createdAt}</div>
                     <div className="ml-2">{author}</div>
+                </div>
+                <div className="flex text-[10px] gap-2 mt-1">
+                    {tags.map((tag) => (
+                        <Tag key={tag} tag={tag} />
+                    ))}
                 </div>
             </div>
             <div className="flex flex-grow justify-end text-right items-center text-sm">

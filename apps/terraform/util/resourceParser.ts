f9525d29b792008c57421e71055ae3d4473b576a
Gdm0714 <ehdals45454@naver.com>
2024-11-21 19:33:27 +0900

    🐞 Fix: json 파싱에서 properties안에 있던 name 분리
    ISSUES CLOSED: #78

--- apps/terraform/util/resourceParser.ts
+++ apps/terraform/util/resourceParser.ts
@@ -12,23 +12,23 @@ import { NCloudPublicIP } from '../model/NCloudPublicIP';
 
 
 export function parseToNCloudModel(resource: CloudCanvasNode ): NCloudModel {
-    const { type, properties } = resource;
+    const { type, name, properties } = resource;
 
     switch (type.toLowerCase()) {
         case 'vpc':
             return new NCloudVPC({
-                name: properties.name || 'vpc',
+                name: name || 'vpc',
                 ipv4CidrBlock: properties.cidrBlock
             });
 
         case 'networkacl':
             return new NCloudNetworkACL({
-                name: properties.name || 'nacl'
+                name: name || 'nacl'
             });
 
         case 'subnet':
             return new NCloudSubnet({
-                name: properties.name || 'subnet',
+                name: name || 'subnet',
                 subnet: properties.subnet,
                 zone: properties.zone,
                 subnetType: properties.subnetType,
@@ -38,7 +38,7 @@ export function parseToNCloudModel(resource: CloudCanvasNode ): NCloudModel {
         case 'acg':
         case 'accesscontrolgroup':
             return new NCloudACG({
-                name: properties.name || 'acg',
+                name: name || 'acg',
                 description: properties.description
             });
 
@@ -53,24 +53,24 @@ export function parseToNCloudModel(resource: CloudCanvasNode ): NCloudModel {
 
         case 'loginkey':
             return new NCloudLoginKey({
-                name: properties.name || 'login-key'
+                name: name || 'login-key'
             });
 
         case 'networkinterface':
             return new NCloudNetworkInterface({
-                name: properties.name || 'nic'
+                name: name || 'nic'
             });
 
         case 'server':
             return new NCloudServer({
-                name: properties.name || 'server',
+                name: name || 'server',
                 serverImageProductCode: properties.serverImageProductCode,
                 serverProductCode: properties.serverProductCode
             });
 
         case 'publicip':
             return new NCloudPublicIP({
-                name: properties.name || 'public-ip'
+                name: name || 'public-ip'
             });
 
         default:

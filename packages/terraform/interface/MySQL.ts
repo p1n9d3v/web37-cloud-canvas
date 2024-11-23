9347bb3f1b99c74a8d30930955ea1c58ed6add2f
Gdm0714 <ehdals45454@naver.com>
2024-11-24 02:05:51 +0900

    ✨ Feat: mySQL 모델 추가

---
+++ packages/terraform/interface/MySQL.ts
@@ -0,0 +1,10 @@
+export interface MySQL {
+    id: string;
+    serviceName: string;
+    serverNamePrefix: string;
+    userName: string;
+    userPassword: string;
+    hostIp: string;
+    databaseName: string;
+    subnetNo: string;
+}

dea334841c466cd1117698ba9e9d3586561d2162
paulcjy <paulcjy@naver.com>
2024-11-24 19:57:51 +0900

    ✨ Feat(server): prisma exception filter 추가

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,9 +1,10 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { swaggerConfig } from './swagger/swagger.config';
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
 import * as cookieParser from 'cookie-parser';
+import { PrismaExceptionFilter } from './filters/prisma-exception.filter';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -23,6 +24,9 @@ async function bootstrap() {
             disableErrorMessages: false,
         }),
     );
+
+    const { httpAdapter } = app.get(HttpAdapterHost);
+    app.useGlobalFilters(new PrismaExceptionFilter(httpAdapter));
 
     await app.listen(3000);
 }
